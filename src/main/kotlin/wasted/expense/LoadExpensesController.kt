b60141cd6e29d4702e38b4166b057db77f6b1dcd
matvey <msmych@yandex.com>
2019-07-04 00:09:54 +0300

    Added loading expenses logging

--- src/main/kotlin/wasted/expense/LoadExpensesController.kt
+++ src/main/kotlin/wasted/expense/LoadExpensesController.kt
@@ -1,6 +1,7 @@
 package wasted.expense
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,10 +15,13 @@ class LoadExpensesController(val objectMapper: ObjectMapper,
                              val mongoSequenceService: MongoSequenceService,
                              val expenseRepository: ExpenseRepository) {
 
+    private val log = LoggerFactory.getLogger(LoadExpensesController::class.java)
+
     @PostMapping("json")
     fun loadJson(@RequestParam file: MultipartFile) {
-        objectMapper.readValue(file.bytes, Expenses::class.java)
-                .expenses
+        val expenses = objectMapper.readValue(file.bytes, Expenses::class.java).expenses
+        log.info("Loading {} expenses", expenses.size)
+        expenses
                 .forEach { expenseRepository.save(
                         Expense(mongoSequenceService.next("expense"),
                                 it.userId,
