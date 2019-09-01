b4c5bc331ac051814b6c81db1df6c92056226dea
matvey <msmych@yandex.com>
2019-09-01 12:40:11 +0300

    Checking SHA-256(user-id api-token)

--- src/main/kotlin/wasted/token/TokenInterceptor.kt
+++ src/main/kotlin/wasted/token/TokenInterceptor.kt
@@ -1,5 +1,6 @@
 package wasted.token
 
+import org.apache.commons.codec.digest.DigestUtils.sha256Hex
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
@@ -10,13 +11,16 @@ import javax.servlet.http.HttpServletResponse.SC_FORBIDDEN
 @Component
 class TokenInterceptor : HandlerInterceptor {
 
-    @Value("\${api-token}")
-    lateinit var apiToken: String
+  @Value("\${api-token}")
+  lateinit var apiToken: String
 
-    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        if (setOf("/user", "/expense").none { request.requestURI.startsWith(it) }) return true
-        if (request.getHeader("api-token") == apiToken) return true
-        response.sendError(SC_FORBIDDEN, "api-token is not valid")
-        return false
-    }
+  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    if (setOf("/user", "/expense", "/total").none { request.requestURI.startsWith(it) }) return true
+    val apiTokenHeader = request.getHeader("api-token")
+    if (apiTokenHeader == apiToken) return true
+    val userIdHeader = request.getHeader("user-id")
+    if (sha256Hex("$userIdHeader$apiTokenHeader") == sha256Hex("$userIdHeader$apiToken")) return true
+    response.sendError(SC_FORBIDDEN, "api-token is not valid")
+    return false
+  }
 }
