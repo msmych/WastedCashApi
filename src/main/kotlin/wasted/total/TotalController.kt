9e2be7723f8765307644a32f2f27cfb60e67b7c6
matvey <msmych@yandex.com>
2019-09-03 22:50:18 +0300

    Changed indent

--- src/main/kotlin/wasted/total/TotalController.kt
+++ src/main/kotlin/wasted/total/TotalController.kt
@@ -16,46 +16,48 @@ import java.util.*
 @RequestMapping("total")
 class TotalController(val expenseRepository: ExpenseRepository) {
 
-    @GetMapping("in/{groupId}/type/{type}")
-    fun getTotal(@PathVariable groupId: Long, @PathVariable type: Type): List<Total> {
-        if (type == ALL)
-            return toTotalList(expenseRepository.findAllByGroupId(groupId))
-        return toTotalList(expenseRepository.findAllByGroupIdAndDateGreaterThanEqual(groupId, date(type)))
-    }
+  @GetMapping("in/{groupId}/type/{type}")
+  fun getTotal(@PathVariable groupId: Long, @PathVariable type: Type): List<Total> {
+    if (type == ALL)
+      return toTotalList(expenseRepository.findAllByGroupId(groupId))
+    return toTotalList(expenseRepository.findAllByGroupIdAndDateGreaterThanEqual(groupId, date(type)))
+  }
 
-    private fun toTotalList(expenses: List<Expense>): List<Total> {
-        return expenses.groupBy { it.currency }
-                .map { cur ->
-                    cur.value.groupBy { it.category }
-                            .map { cat -> cat.value
-                                    .map { Total(it.groupId, it.userId, it.amount, cur.key, cat.key) }
-                                    .reduce { acc, total -> Total(
-                                            total.groupId,
-                                            total.userId,
-                                            acc.amount + total.amount,
-                                            total.currency,
-                                            total.category)
-                                    }
-                            }
-                }.flatten()
-    }
+  private fun toTotalList(expenses: List<Expense>): List<Total> {
+    return expenses.groupBy { it.currency }
+      .map { cur ->
+        cur.value.groupBy { it.category }
+          .map { cat ->
+            cat.value
+              .map { Total(it.groupId, it.userId, it.amount, cur.key, cat.key) }
+              .reduce { acc, total ->
+                Total(
+                  total.groupId,
+                  total.userId,
+                  acc.amount + total.amount,
+                  total.currency,
+                  total.category)
+              }
+          }
+      }.flatten()
+  }
 
-    private fun date(type: Type): Date {
-        return Date.from(when (type) {
-            MONTH -> now().withDayOfMonth(1)
-            else -> throw IllegalArgumentException()
-        }
-                .withHour(0)
-                .withMinute(0)
-                .withSecond(0)
-                .toInstant())
+  private fun date(type: Type): Date {
+    return Date.from(when (type) {
+      MONTH -> now().withDayOfMonth(1)
+      else -> throw IllegalArgumentException()
     }
+      .withHour(0)
+      .withMinute(0)
+      .withSecond(0)
+      .toInstant())
+  }
 
-    @GetMapping("type/{type}")
-    fun totals(@PathVariable type: Type): List<Total> {
-        if (type == ALL) {
-            return toTotalList(expenseRepository.findAll())
-        }
-        return toTotalList(expenseRepository.findAllByDateGreaterThanEqual(date(type)))
+  @GetMapping("type/{type}")
+  fun totals(@PathVariable type: Type): List<Total> {
+    if (type == ALL) {
+      return toTotalList(expenseRepository.findAll())
     }
+    return toTotalList(expenseRepository.findAllByDateGreaterThanEqual(date(type)))
+  }
 }
